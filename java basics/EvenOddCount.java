e24109bac1f9a45b04efcd094580601da85de583
Purvi <gupta.purvi.2206@gmail.com>
2026-08-18 22:31:28 +0530

    Array basics

---
+++ java basics/EvenOddCount.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class EvenOddCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        sc.close();
+        int[] arr = new int[n];
+        int even = 0;
+        int odd = 0;
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+
+            if (arr[i] % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+
+        System.out.println("Even numbers = " + even);
+        System.out.println("Odd numbers = " + odd);
+    }
+}
