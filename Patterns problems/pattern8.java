b6b331d431801d345a1aeba73ab38e6680a755ec
Purvi <gupta.purvi.2206@gmail.com>
2026-08-22 23:13:23 +0530

    pattern continue

---
+++ Patterns problems/pattern8.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class pattern8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
